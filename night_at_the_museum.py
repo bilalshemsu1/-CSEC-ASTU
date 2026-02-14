a8c8ef83da0a54d73a4b7cef8e5bdcd3da752255
Bilal Shemsu <150489466+bilalshemsu1@users.noreply.github.com>
2026-02-14 10:34:25 -0900

    Add files via upload

---
+++ night_at_the_museum.py
@@ -0,0 +1,13 @@
+word = input()
+
+current = 0
+total = 0
+
+for ch in word:
+    target = ord(ch)-ord('a')
+    diff = abs(target - current)
+    total +=min(diff, 26-diff)
+    current=target
+
+print(total) 
+
